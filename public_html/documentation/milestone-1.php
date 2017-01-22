b343b9fe77bd48a873aa2187273924f8afc2b2ba
Brandon Steider <bsteider@cnm.edu>
2017-01-21 18:22:33 -0700

    Added main body

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<titls>Brandon Steider Personal Website</titls>
+	</head>
+	<body>
+			<h2>Purpose</h2>
+		<ul>I want my PWP to show me as a coder. How good I will be, what I bring to the table, and overall act as a type of resume. Beyond that I want to do all kinds of work on it to explore what different possibilites I can have making a website and see how creative I can get with it. I want it to be my first accomplishment in the class.</ul>
+			<h2>Audience</h2>
+		<ul>Primarily I want my PWP to be viewed by potential employers or customers. If i start working for someone they should know what kind of work I am capable of creating. That goes as well for customers if I do freelance work. They should be able to see that I know what i'm doing and can make all kinds of different things. </ul>
+			<h2>Goal</h2>
+		<ul>My goal is to aquire a full time job.</ul>
+			<h1>persona</h1>
+		<ul>Name: David Hampton</ul>
+		<ul>Age: 29</ul>
+		<ul>Owner of coding company</ul>
+		<ul>Uses Windows Computer</ul>
+		<ul>Needs: Web developer who can do frontend or backend coding to work for his company. The age range of applicant does not matter. The main concner is they need to be able to learn, and complete work as assigned. </ul>
+			<h2>Goal</h2>
+		<p>David would be using my site to assess what exactly I can do as far as web development. If i'm creative, a hard worker, and if I stand out from the other people he has considered for the position.</p>
+		<h2>Frustrations</h2>
+			<p>David is frustrated because he has gone through a long list of applicants and hasn't been very impressed so far. After viewing my website he finds himself in awe because of it's true beauty.</p>
+	</body>
+</html>

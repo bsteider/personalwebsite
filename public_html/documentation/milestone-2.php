91c995f5a0fc76357d75910a2b66bb86e24a2594
Brandon Steider <bsteider@cnm.edu>
2017-02-05 17:19:54 -0700

    added body and image to milestone 2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,26 +1,15 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Brandon Steider Personal Website</title>
-	</head>
+	<head><title>Brandon Steider Milestone 2</title></head>
 	<body>
-		<h2>Purpose</h2>
-		<p>I want my PWP to show me as a coder. How good I will be, what I bring to the table, and overall act as a type of resume. Beyond that I want to do all kinds of work on it to explore what different possibilites I can have making a website and see how creative I can get with it. I want it to be my first accomplishment in the class.</p>
-		<h2>Audience</h2>
-		<p>Primarily I want my PWP to be viewed by potential employers or customers. If i start working for someone they should know what kind of work I am capable of creating. That goes as well for customers if I do freelance work. They should be able to see that I know what i'm doing and can make all kinds of different things. </p>
-		<h2>Goal</h2>
-		<p>My goal is to aquire a full time job.</p>
-		<h1>persona</h1>
-		<ul>Name: David Hampton</ul>
-		<ul>name: david humpty</ul>
-		<!-- test -->
-		<ul>Age: 29</ul>
-		<ul>Owner of coding company</ul>
-		<ul>Uses Windows Computer</ul>
-		<ul>Needs: Web developer who can do frontend or backend coding to work for his company. The age range of applicant does not matter. The main concner is they need to be able to learn, and complete work as assigned. </ul>
-		<h2>Goal</h2>
-		<p>David would be using my site to assess what exactly I can do as far as web development. If i'm creative, a hard worker, and if I stand out from the other people he has considered for the position.</p>
-		<h2>Frustrations</h2>
-		<p>David is frustrated because he has gone through a long list of applicants and hasn't been very impressed so far. After viewing my website he finds himself in awe because of it's true beauty.</p>
+		<h2>Content</h2>
+		<ui>Links/pictures of my previous projects and work</ui>
+		<ul>Information about who I am</ul>
+		<ui>The skills I have as a web developer </ui>
+		<ui>What I plan to acheive as a web developer</ui>
+		<ui>Ways to contact me</ui>
+		<h2>Purpose of content</h2>
+		<ui>I decided to choose four main points for my content. I want to keep it short and sweet, but look good and have the important information. I want my work on there to show potential employers who what sort of stuff I can create. The purpose of the "who I am" is to give them a general idea of what kind of person I am. The skills tell people what i'm able to create, what languages I can code, and basically cover the work I do. The last section will state what my plans are, like if I want to persue a career in this field, start my own company, etc.</ui>
+		<img src="wirefram milestone 2.png" alt="wireframe">
 	</body>
 </html>

34ae9b34a38cdfb03f0d12e3f55669116ba631bf
Brandon Steider <bsteider@cnm.edu>
2017-02-28 13:18:21 -0700

    added image, messing with text

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Pridi" rel="stylesheet">
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
@@ -43,8 +45,11 @@
 				</div><!-- /.navbar-collapse -->
 			</div><!-- /.container-fluid -->
 		</nav>
+		<div>
+		<img src="/img/brandonPicture.jpg" alt="picture of brando" class="img-rounded">
+		</div>
 		<div class="boxed">
-			This text is enclosed in a box.
+			Taxidermy godard four loko tumblr cliche. Offal readymade af humblebrag, vaporware cardigan chartreuse VHS post-ironic succulents four dollar toast tumeric. Pug pour-over kitsch literally, subway tile edison bulb direct trade woke vegan authentic cray banh mi tacos. Meggings iPhone bitters gluten-free, typewriter schlitz meditation tumeric sriracha 90's mustache authentic. Blue bottle franzen disrupt hoodie hammock locavore. Franzen wayfarers ugh meh, banjo banh mi asymmetrical before they sold out small batch street art synth biodiesel lyft farm-to-table kitsch. Before they sold out listicle skateboard vexillologist kitsch, tumblr blue bottle vape. Mumblecore whatever artisan narwhal, man braid four loko XOXO tattooed twee vexillologist meggings seitan jean shorts messenger bag VHS. Kale chips sriracha waistcoat cornhole. Paleo squid whatever aesthetic. Tumeric brooklyn man braid, street art fashion axe tote bag freegan. Subway tile bespoke copper mug, polaroid occupy marfa 8-bit kale chips butcher vegan drinking vinegar shoreditch cronut. Chartreuse actually YOLO, vaporware literally roof party meggings cred artisan snackwave skateboard asymmetrical slow-carb. Green juice post-ironic humblebrag, unicorn vice pok pok mustache mixtape fanny pack kogi vape readymade gastropub iceland sartorial. Vexillologist cardigan enamel pin squid, food truck keytar pop-up viral tote bag yuccie 3 wolf moon narwhal air plant readymade. Bicycle rights lo-fi wayfarers shoreditch pour-over kogi plaid taxidermy, shabby chic mumblecore. Tattooed taxidermy raclette plaid. Etsy bushwick man braid next level, prism master cleanse coloring book. Man bun butcher microdosing vice, semiotics listicle messenger bag seitan polaroid pop-up small batch. Locavore blog disrupt chillwave, organic literally deep v vaporware. Pabst raw denim yuccie, fixie live-edge mustache drinking vinegar raclette glossier.
 		</div>
 
 		<header>
